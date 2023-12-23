a4a46e3b12210f6da1a0981e1bc72259711af8f8
rbpolim <rbpolim@gmail.com>
2023-12-23 09:54:55 -0300

    test: input component

--- src/components/Input/input.test.tsx
+++ src/components/Input/input.test.tsx
@@ -3,7 +3,15 @@ import { render } from "@testing-library/react-native";
 import { Input } from "@components/Input";
 
 describe("Component: Input", () => {
-  it("should be render correctly", () => {
-    // TODO: Complete this test
+  it("should not render the activity indicator when isLoading is false", () => {
+    const { queryByTestId } = render(<Input />);
+
+    expect(queryByTestId(/activity-indicator/i)).toBeNull();
+  });
+
+  it("should render the activity indicator when isLoading is true", () => {
+    const { getByTestId } = render(<Input isLoading />);
+
+    expect(getByTestId(/activity-indicator/i)).toBeTruthy();
   });
 });
