df1f554e5b5c57d11a09397af453d47b4e4077fd
rbpolim <rbpolim@gmail.com>
2023-12-24 16:44:06 -0300

    test: service getWeatherByCity & mock file

---
+++ src/services/getWeatherByCityService.test.ts
@@ -0,0 +1,18 @@
+import { api } from './api'
+import { mockWeatherAPIResponse } from './mocks'
+import { getWeatherByCityService } from './getWeatherByCityService'
+
+describe('Service: getWeatherByCityService', () => {
+  it('should returns the weather data API', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({
+      data: mockWeatherAPIResponse,
+    })
+
+    const response = await getWeatherByCityService({
+      latitude: 123,
+      longitude: 321,
+    })
+
+    expect(response).toHaveProperty('batata')
+  })
+})
