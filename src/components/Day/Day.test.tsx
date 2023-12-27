4215b15a04b321fdf936dd56d46af096ce559a78
rbpolim <rbpolim@gmail.com>
2023-12-27 10:05:25 -0300

    test: day component

---
+++ src/components/Day/Day.test.tsx
@@ -0,0 +1,20 @@
+import { render } from '@testing-library/react-native'
+
+import { Day } from '@components/Day'
+import clearDay from '@assets/clear_day.svg'
+
+describe('Day', () => {
+  it('renders Day component', () => {
+    const data = {
+      day: '18/07',
+      min: '15',
+      max: '20',
+      icon: clearDay,
+      weather: 'Ensolarado',
+    }
+
+    const { getByText } = render(<Day data={data} />)
+
+    expect(getByText('18/07')).toBeTruthy()
+  })
+})
